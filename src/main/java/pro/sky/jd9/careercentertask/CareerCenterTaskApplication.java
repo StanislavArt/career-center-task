f720808a4d867ceae60cf778fe831106125184a7
Stanislav <artemiev.stanislav@gmail.com>
2023-05-16 18:35:16 +0700

    Initial

---
+++ src/main/java/pro/sky/jd9/careercentertask/CareerCenterTaskApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.jd9.careercentertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerCenterTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerCenterTaskApplication.class, args);
+	}
+
+}
